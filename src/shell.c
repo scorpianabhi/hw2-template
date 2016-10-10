bd65d85b245d78f4a06d333172203c602cbb7f79
scorpianabhi <khandraabhigna@gmail.com>
2016-10-09 22:01:21 -0700

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -21,41 +21,41 @@ int addCommandToHistory(char addCommand[], int current) //void to int
   //TODO: add the command into the history here.
   int temp;
   if (current == 0)
-    exit(0);
-  if (current < 0)
-  {
-    perror("error reading the command");
-    exit(-1);           /* terminate with error code of -1 */
-  }
+    exit(0);  
+    if (current < 0)
+    {
+        perror("error reading the command");
+        exit(-1);           /* terminate with error code of -1 */
+    }
   else
-  {
+    {
     addCommand[current]='\0';
     if(addCommand[0]=='r')
-    {
-      if(addCommand[1]=='r')
-      {
-        if(g_command_count==0)
         {
-          printf("No recent command can be found in the history. \n");
-          return 0;
-        }
-        strcpy(addCommand,g_history[(g_command_count)% HISTORY_SIZE]);
-      }
-      else
-      {
-        temp = atoi(&addCommand[1]);
-        if(temp < 1 || temp > g_command_count || temp <= g_command_count -HISTORY_SIZE)
+        if(addCommand[1]=='r')
+            {
+            if(g_command_count==0)
+                {
+                printf("No recent command can be found in the history. \n");
+                return 0;
+                }
+            strcpy(addCommand,g_history[(g_command_count)% HISTORY_SIZE]);
+            }
+        else
         {
-          printf("Command number cannot be found. \n");
-          return 0;
+            temp = atoi(&addCommand[1]);
+            if(temp < 1 || temp > g_command_count || temp <= g_command_count -HISTORY_SIZE)
+                {
+                printf("Command number cannot be found. \n");
+                return 0;
+                }
+            strcpy(addCommand,g_history[(temp-1)%HISTORY_SIZE]);
         }
-        strcpy(addCommand,g_history[(temp-1)%HISTORY_SIZE]);
-      }
-      current = strlen(addCommand);
-    }
+        current = strlen(addCommand);
+            }
     g_command_count++;
     strcpy(g_history[(g_command_count-1)%HISTORY_SIZE], addCommand);
-  }
+}
 }
 
 /**
@@ -86,23 +86,23 @@ void displayHistory()
 int setup(char inputBuffer[], char *args[])
 {
   int m_inputLength,                //Number of characters that read() reads from the command line
-          i,                            //Arbitrary counter
-          start,                        //temporary index ( helper) used to indicate the start of the next parameter
-          argumentCharacterCounter;     //counter index for where to place the next parameter into args[]
+      i,                            //Arbitrary counter
+      start,                        //temporary index ( helper) used to indicate the start of the next parameter
+      argumentCharacterCounter;     //counter index for where to place the next parameter into args[]
   /**
    * Printing shell> and reading in from the standard input
    */
-  do {
+    do {
     printf("shell>");
     fflush(0);  //Flush 'shell>' out into the command line
     m_inputLength = read(STDIN_FILENO, inputBuffer, MAX_CHARACTERS_LINE);  //Read from command line into our inputBuffer
-  }while(inputBuffer[0] != '\n');
+    }while(inputBuffer[0] != '\n');
   /**
    * Add the command to the history
    */
   addCommandToHistory(inputBuffer, m_inputLength);
 
-  int temp;
+    int temp;
   argumentCharacterCounter = 0;  //helper variable
   start = -1;                   //helper variable.
 
@@ -118,49 +118,49 @@ int setup(char inputBuffer[], char *args[])
 
     switch (inputBuffer[i])
     {
-      case ' ':
-      case '\t' :               /* argument separators */
-        if(start != -1)
-        {
-          args[argumentCharacterCounter] = &inputBuffer[start];    /* set up pointer */
-          argumentCharacterCounter++;
-        }
+    case ' ':
+        case '\t' :               /* argument separators */
+            if(start != -1)
+            {
+                args[argumentCharacterCounter] = &inputBuffer[start];    /* set up pointer */
+                argumentCharacterCounter++;
+            }
             inputBuffer[i] = '\0'; /* add a null char; make a C string */
             start = -1;
             break;
 
-      case '\n':                 /* should be the final char examined */
-        if (start != -1){
-          args[argumentCharacterCounter] = &inputBuffer[start];
-          argumentCharacterCounter++;
-        }
+        case '\n':                 /* should be the final char examined */
+            if (start != -1){
+                args[argumentCharacterCounter] = &inputBuffer[start];
+                argumentCharacterCounter++;
+            }
             inputBuffer[i] = '\0';
             args[argumentCharacterCounter] = NULL; /* no more arguments to this command */
             break;
 
-      case '&':
-        // *background = 1;
-        inputBuffer[i] = '\0';
+        case '&':
+            // *background = 1;
+            inputBuffer[i] = '\0';
             break;
 
-      default :             /* some other character */
-        if (start == -1)
-          start = i;
+        default :             /* some other character */
+            if (start == -1)
+                start = i;
     }
   }
 
   args[argumentCharacterCounter] = NULL; //just in case the input line was > 80
-  for (i = 0; i < m_inputLength; i++)
-  {
-    if (inputBuffer[i] == '&')
+    for (i = 0; i < m_inputLength; i++)
     {
-      inputBuffer[i] = '\0';
-      // *background = 1;
-      --inputBuffer;
-      break;
-    }
+        if (inputBuffer[i] == '&')
+        {
+            inputBuffer[i] = '\0';
+            // *background = 1;
+            --inputBuffer;
+            break;
+        }
 
-  }
+    }
   return 1;
 
 }  //End of setup()
@@ -189,36 +189,36 @@ int shell(int argc, char *argv[])
     // Start the external command execution here
     if (m_isValidCommand) {
       //TODO: use fork to create a child
-      m_child = fork();
-      if(m_child < 0)
-      { /* if the child process didn't return 0, the fork is failed */
+          m_child = fork(); 
+          if(m_child < 0)
+    { /* if the child process didn't return 0, the fork is failed */
         printf("Fork failed! \n");
-      }
-      else if(m_child==0)
-      { /* child process */
+    }
+else if(m_child==0)
+        { /* child process */
         if(m_inputBuffer[0]=='history' || m_inputBuffer[0] =='h')
-          printf("I am the child %d\n",m_child);
-        execlp("/bin/ls","ls",NULL);
-        {
-          displayHistory();
-          else
+	printf("I am the child %d\n",pid);
+    	execlp("/bin/ls","ls",NULL);
+	{
+            displayHistory();
+//else
 
-          { /* parent process */
-            /* parent will wait for the child to complete */
-            printf("I am the parent %d\n",m_child);
-            wait(NULL);
+	{ /* parent process */
+    /* parent will wait for the child to complete */
+    printf("I am the parent %d\n",pid);
+    wait(NULL);
 
-            printf("Child Complete\n");
-          }
-          return 0;
+    printf("Child Complete\n");
+  }
+            return 0;
         }
         m_status = execvp(args[0],args);
         if(m_status !=0){
-          printf("%s: command not found. \n", args[0]);
+            printf("%s: command not found. \n", args[0]);
         }
 
-      }
-      /* create a child process*/
+    }
+          /* create a child process*/
       //TODO: use perror() to indicate errors
       //TODO: use execvp inside the child to start the program passed in as argument
       //TODO: make the parent wait for the child
